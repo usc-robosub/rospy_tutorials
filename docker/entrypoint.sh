756b3223066e0c3057d4c4cca3279dd4d6e40ca7
Alejandro Arteaga <ajarteag@usc.edu>
2026-03-02 11:51:51 -0800

    cpp_pubsub added

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 
 source /opt/ros/humble/setup.bash
 
-echo "Installing dependencies..."
-apt-get update
-rosdep install --from-paths src --ignore-src -y
+# echo "Installing dependencies..."
+# apt-get update
+# rosdep install --from-paths src --ignore-src -y
 
 # echo "Building ROS 2 workspace..."
 # colcon build --symlink-install
