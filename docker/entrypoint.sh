f5cc0851a5411a0b1747f9eacf5c34504a5eaa38
Alejandro Arteaga <ajarteag@usc.edu>
2026-03-02 15:47:38 -0800

    fixed dockerfiles to build ros2 workspace while building image instead of starting up container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,14 +2,7 @@
 set -e
 
 source /opt/ros/humble/setup.bash
-
-echo "Installing dependencies..."
-apt-get update
-rosdep install --from-paths src --ignore-src -y
-
-echo "Building ROS 2 workspace..."
-colcon build --symlink-install
-source install/setup.bash
+source /ros2_ws/install/setup.bash
 
 echo "=========================================="
 echo " ROS 2 Humble Tutorial Workspace Ready! "
